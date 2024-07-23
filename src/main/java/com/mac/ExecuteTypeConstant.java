9768ad55a3794fb3f2e49e5304f1ae54d37a1557
macjay <32384242+macjay007@users.noreply.github.com>
2024-07-23 14:25:48 +0800

    Create ExecuteTypeConstant.java

---
+++ src/main/java/com/mac/ExecuteTypeConstant.java
@@ -0,0 +1,23 @@
+package com.mac.log;
+
+/**
+*
+* @author zj
+* @Date 2024/7/23 10:30
+**/
+public class ExecuteTypeConstant {
+    public static final String ADD = "ADD";
+    public static final String DELETE = "DELETE";
+    public static final String UPDATE = "UPDATE";
+    public static final String SELECT = "SELECT";
+    public static final String LOGIN = "LOGIN";
+    public static final String IMPORT = "IMPORT";
+    public static final String EXPORT = "EXPORT";
+    public static final String UPLOAD = "UPLOAD";
+    public static final String DOWNLOAD = "DOWNLOAD";
+    public static final String OTHER = "OTHER";
+    public static final String LOGOUT = "LOGOUT";
+
+    public ExecuteTypeConstant() {
+    }
+}
